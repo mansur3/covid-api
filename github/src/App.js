2df5bdf2eddee8842398843a0c29b90cdc81303a
Mansur Dewan <mansurdewan26@gmail.com>
2021-10-27 18:55:14 +0530

    git hub

---
+++ github/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import {Github} from "./components/Github";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Github />
+    </div>
+  );
+}
+
+export default App;
